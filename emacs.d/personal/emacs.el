c5c1c00b21e0be7502be2a1b4d4bfdd2824e1e97
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-11-26 15:33:10 -0500

    adds packages to install

--- emacs.d/personal/emacs.el
+++ emacs.d/personal/emacs.el
@@ -71,11 +71,27 @@
 ;;(defvaralias 'c-basic-offset 'tab-width)
 ;;(defvaralias 'cperl-indent-level 'tab-width)
 
-
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(emamux auto-complete less-css-mode moe-theme
+                             multi-web-mode markdown-mode textmate web-mode
+                             handlebars-mode magit clojure-mode
+                             pastels-on-dark-theme websocket pony-mode
+                             org go-mode powerline minitest rspec-mode
+                             ruby-block ruby-end ruby-additional
+                             ctags-update go-autocomplete virtualenv))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 
 (provide 'emacs.el)
