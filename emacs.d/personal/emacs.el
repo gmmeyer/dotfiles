d29f948352686b592abc6f0c68522ce4ad51122e
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-12-08 15:25:50 -0500

    better indenting for emacs

--- emacs.d/personal/emacs.el
+++ emacs.d/personal/emacs.el
@@ -58,8 +58,13 @@
 (setq-default css-indent-offset 2)
 (setq-default sh-basic-offset 2)
 (setq-default js2-basic-offset 2)
-(setq-default python-indent 2)
-(setq-default python-indent-offset 2)
+
+(setq-default python-indent 4)
+(setq-default python-indent-offset 4)
+
+(setq-default web-mode-markup-indent-offset 2)
+(setq-default web-mode-css-indent-offset 2)
+(setq-default web-mode-code-indent-offset 2)
 
 (setq cssm-indent-level 2)
 (setq cssm-newline-before-closing-bracket t)
