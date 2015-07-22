c549bb28d620e09ed3920fcd55a62fe5e3124238
Greg Meyer <gregory.matthew.meyer@gmail.com>
2015-07-22 02:32:04 -0400

    moe theme stuff

--- emacs.d/personal/appearance.el
+++ emacs.d/personal/appearance.el
@@ -1,29 +1,41 @@
 ;;;Set up the appearance of emacs
 
-;; powerline > mode line
-;;(require 'powerline)
+
 ;;(powerline-default-theme)
 
 (disable-theme 'zenburn)
-(require 'moe-theme)
 
-(moe-dark)
+(menu-bar-mode -1)
 
 (setq-default visible-cursor nil)
-
 (blink-cursor-mode (- (*) (*) (*)))
 
-(moe-theme-set-color 'w/b)
-
-(require 'powerline)
-(powerline-moe-theme)
+(show-paren-mode t)
 
 (global-hl-line-mode 0)
 
-(show-paren-mode t)
+;; moe theme stuff
+
+;; powerline > mode line
+(require 'powerline)
+
+(require 'moe-theme)
+
+(setq moe-theme-highlight-buffer-id t)
+
+(powerline-moe-theme)
+
+;; markdown title resizing
+(setq moe-theme-resize-markdown-title '(2.0 1.7 1.5 1.3 1.0 1.0))
+(setq moe-theme-resize-org-title '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
+(setq moe-theme-resize-rst-title '(2.0 1.7 1.5 1.3 1.1 1.0))
+
 (setq show-paren-style 'expression)
 
-(menu-bar-mode -1)
+(moe-theme-set-color 'w/b)
+
+(moe-dark)
+
 ;;(load-theme 'railscasts)
 
 (provide 'appearance.el)
