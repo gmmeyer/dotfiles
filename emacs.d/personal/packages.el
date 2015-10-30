44bb870f531aacc29b0966d39402309b91aea39e
Greg Meyer <gregory.matthew.meyer@gmail.com>
2015-10-30 15:16:38 -0400

    stuff

--- emacs.d/personal/packages.el
+++ emacs.d/personal/packages.el
@@ -2,9 +2,9 @@
 
 ;;; Commentary: my packages
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
