030f3062ecc95d66dce5b266fb637680fae665c4
Greg Meyer <gregory.matthew.meyer@gmail.com>
2016-04-18 18:39:29 -0400

    updates editor config

--- emacs.d/personal/codeprefs.el
+++ emacs.d/personal/codeprefs.el
@@ -31,5 +31,12 @@
         ("mako"    . "\\.mako\\'"))
       )
 
+;; UTF-8 EVERYWHERE!!!
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 (provide 'codeprefs)
 ;;; codeprefs.el ends here
