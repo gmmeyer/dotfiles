7c20165a972696bb298e21bdeeb5a0c320683e0f
Greg Meyer <greg.meyer@datadoghq.com>
2016-12-22 13:01:48 -0500

    some environment changes

--- zsh/environment/history.zsh
+++ zsh/environment/history.zsh
@@ -2,18 +2,18 @@ HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"
 HISTSIZE=10000
 SAVEHIST=10000
 
-
-setopt BANG_HIST
-setopt EXTENDED_HISTORY
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_FIND_NO_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_SAVE_NO_DUPS
-setopt HIST_VERIFY
-setopt HIST_BEEP
+setopt append_history
+setopt bang_hist
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_find_no_dups
+setopt hist_ignore_space
+setopt hist_save_no_dups
+setopt hist_verify
+setopt hist_beep
+setopt inc_append_history
+setopt share_history
 
 alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
