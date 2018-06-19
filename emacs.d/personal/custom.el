d743c863fc8f22533b0d3b68d2b51d464de16ce8
Greg Meyer <greg.meyer@datadoghq.com>
2018-06-19 15:28:28 -0400

    updates

--- emacs.d/personal/custom.el
+++ emacs.d/personal/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
    (quote
     ("685a7460fdc4b8c38796234d3a96b3aacbe4fba739fb33b5d6d149051ce74a58" "cb8d13429234ff2a8700da4db9bdf6b952c1b54b906a1aad2d0d98317c5b0224" default)))
