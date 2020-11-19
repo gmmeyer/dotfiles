725a4eeaf60231abee0b990447e1b70afa496e4d
Greg Meyer <greg.meyer@datadoghq.com>
2020-11-18 20:06:07 -0500

    updates

--- emacs.d/personal/custom.el
+++ emacs.d/personal/custom.el
@@ -6,25 +6,20 @@
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
-   (quote
-    ("685a7460fdc4b8c38796234d3a96b3aacbe4fba739fb33b5d6d149051ce74a58" "cb8d13429234ff2a8700da4db9bdf6b952c1b54b906a1aad2d0d98317c5b0224" default)))
+   '("685a7460fdc4b8c38796234d3a96b3aacbe4fba739fb33b5d6d149051ce74a58" "cb8d13429234ff2a8700da4db9bdf6b952c1b54b906a1aad2d0d98317c5b0224" default))
  '(fci-rule-color "#383838")
  '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(package-selected-packages
-   (quote
-    (scala-mode merlin ido-completing-read+ ht hydra sesman lsp-ui company-lsp counsel swiper ivy cask-mode pyenv-mode-auto pyenv-mode zop-to-char zenburn-theme yari yaml-mode which-key websocket web-mode volatile-highlights vkill virtualenv utop undo-tree tuareg textmate smex smartrep smartparens smart-mode-line slime scss-mode scala-mode2 ruby-tools ruby-block rspec-mode rainbow-mode rainbow-delimiters powerline pony-mode pastels-on-dark-theme paradox ov operate-on-number multi-web-mode multi-term move-text moe-theme minitest markdown-mode magit less-css-mode json-mode js2-mode inf-ruby imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag haskell-mode guru-mode grizzl gotest god-mode go-projectile go-autocomplete gitignore-mode gitconfig-mode git-timemachine gist geiser fzf flycheck-ocaml flx-ido expand-region exec-path-from-shell ensime emamux elisp-slime-nav easy-kill dockerfile-mode discover-my-major diminish diff-hl ctags-update csv-mode crux company-jedi company-go company-anaconda coffee-mode cider browse-kill-ring beacon anzu ace-window)))
+   '(scala-mode merlin ido-completing-read+ ht hydra sesman lsp-ui company-lsp counsel swiper ivy cask-mode pyenv-mode-auto pyenv-mode zop-to-char zenburn-theme yari yaml-mode which-key websocket web-mode volatile-highlights vkill virtualenv utop undo-tree tuareg textmate smex smartrep smartparens smart-mode-line slime scss-mode scala-mode2 ruby-tools ruby-block rspec-mode rainbow-mode rainbow-delimiters powerline pony-mode pastels-on-dark-theme paradox ov operate-on-number multi-web-mode multi-term move-text moe-theme minitest markdown-mode magit less-css-mode json-mode js2-mode inf-ruby imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag haskell-mode guru-mode grizzl gotest god-mode go-projectile go-autocomplete gitignore-mode gitconfig-mode git-timemachine gist geiser fzf flycheck-ocaml flx-ido expand-region exec-path-from-shell ensime emamux elisp-slime-nav easy-kill dockerfile-mode discover-my-major diminish diff-hl ctags-update csv-mode crux company-jedi company-go company-anaconda coffee-mode cider browse-kill-ring beacon anzu ace-window))
  '(paradox-github-token t)
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(safe-local-variable-values
-   (quote
-    ((projectile-project-name . dogweb)
-     (whitespace-line-column . 100))))
+   '((projectile-project-name . dogweb)
+     (whitespace-line-column . 100)))
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
+   '((20 . "#BC8383")
      (40 . "#CC9393")
      (60 . "#DFAF8F")
      (80 . "#D0BF8F")
@@ -41,7 +36,7 @@
      (300 . "#7CB8BB")
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
+     (360 . "#DC8CC3")))
  '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
